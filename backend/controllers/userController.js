c34ac4734999acf4d831fcf74594272b17955dd1
mongarerobert3 <mongarerobert3@gmail.com>
2023-05-04 13:09:52 +0300

    user model update

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const generateToken = require("../config/generateToken")
+
+
+const createUser = asyncHandler(async (req, res) => {
+    const { }
+});
+
+module.exports = {createUser}
