f02ab1070c22bd33437a8fd9e549cf59ebe32ed0
mongarerobert3 <mongarerobert3@gmail.com>
2023-05-26 17:54:27 +0300

    travel

--- frontend/src/components/dashboard/NavbarDashboard.jsx
+++ frontend/src/components/dashboard/NavbarDashboard.jsx
@@ -38,25 +38,18 @@ const NavbarDashboard = () => {
     <div>
       <header id="header" className="header fixed-top d-flex align-items-center">
         <div className="container d-flex align-items-center justify-content-between">
-          <a href="index.html" className="logo d-flex align-items-center me-auto me-lg-0">
-            <img src={logo} alt="logo" className="w-60 h-60" />
-            <h1>Tours and <span>Travel</span></h1>
-          </a>
 
           <nav id="navbar" className="navbar">
             <ul>
-              <li>Hello, {firstName}!</li>
+              <li className='hello'>Hello, <span className='name-text'>{firstName}!</span></li>
               <li><a href="#about">Search</a></li>
               <li><a href="#menu">Tours</a></li>
               <li><a href="#events">Hot Deals<i className="bx bxs-star bx-tada red-icon"></i></a></li>
             </ul>
           </nav>
-          {avatar && <img 
-            className='
-              vertical-align: middle;
-              width: 50px;
-              height: 50px;
-              border-radius: 50%; '
+          <i className="notification bx bxs-bell" style={{ color: '#ce1312' }}></i>
+                    {avatar && <img 
+            className='avatar'
             src={avatar}
             alt="avatar" />}
           <i className="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
